73c2898e91eb7466402858831ee38c754cb42480
Connor Lane Smith <cls@lubutu.com>
2011-05-27 11:13:38 +0100

    typo

--- tail.c
+++ tail.c
@@ -1,4 +1,4 @@
-/* See LICENSE file for copyright and license detaketails. */
+/* See LICENSE file for copyright and license details. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
