a8ef54bae11a3d63d2f4f1fbc488af6416644a5d
sin <sin@2f30.org>
2013-10-05 15:04:50 +0100

    Simplify yes(1)
    Just pick the first argument if multiple are provided.

--- yes.c
+++ yes.c
@@ -8,24 +8,13 @@ static void usage(void);
 int
 main(int argc, char *argv[])
 {
-	char *s = "y";
-
 	ARGBEGIN {
 	default:
 		usage();
 	} ARGEND;
 
-	switch(argc) {
-	case 1:
-		s = argv[0];
-		/* fallthrough */
-	case 0:
-		for(;;)
-			puts(s);
-		break;
-	default:
-		usage();
-	}
+	for (;;)
+		puts(argc >= 1 ? argv[0] : "y");
 	return EXIT_FAILURE; /* should not reach */
 }
 
