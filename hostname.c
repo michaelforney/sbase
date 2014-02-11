3bf464c1d62176a2f78924ff32b220d3893677bd
sin <sin@2f30.org>
2014-02-11 10:59:53 +0000

    Use HOST_NAME_MAX in hostname(1)

--- hostname.c
+++ hostname.c
@@ -1,8 +1,9 @@
 /* See LICENSE file for copyright and license details. */
-#include <unistd.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "util.h"
 
 static void
@@ -14,7 +15,7 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
-	char buf[BUFSIZ];
+	char host[HOST_NAME_MAX + 1];
 
 	ARGBEGIN {
 	default:
@@ -22,9 +23,9 @@ main(int argc, char *argv[])
 	} ARGEND;
 
 	if (argc < 1) {
-		if (gethostname(buf, sizeof(buf)) < 0)
+		if (gethostname(host, sizeof(host)) < 0)
 			eprintf("gethostname:");
-		puts(buf);
+		puts(host);
 	} else {
 		if (sethostname(argv[0], strlen(argv[0])) < 0)
 			eprintf("sethostname:");
