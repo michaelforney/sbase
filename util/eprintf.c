24a5006f57193bc6f65e21eff3cd4ab3122114c1
sin <sin@2f30.org>
2014-03-28 15:55:08 +0000

    Prefix error/warning messages with program name if DEBUG is defined

--- util/eprintf.c
+++ util/eprintf.c
@@ -33,7 +33,9 @@ enprintf(int status, const char *fmt, ...)
 void
 venprintf(int status, const char *fmt, va_list ap)
 {
+#ifdef DEBUG
 	fprintf(stderr, "%s: ", argv0);
+#endif
 
 	vfprintf(stderr, fmt, ap);
 
@@ -50,7 +52,9 @@ weprintf(const char *fmt, ...)
 {
 	va_list ap;
 
+#ifdef DEBUG
 	fprintf(stderr, "%s: ", argv0);
+#endif
 
 	va_start(ap, fmt);
 	vfprintf(stderr, fmt, ap);
