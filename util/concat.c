a62eda512c688e4dada99cba21788cb67258512b
sin <sin@2f30.org>
2014-07-04 16:42:31 +0100

    Don't require two C-d to exit the loop

--- util/concat.c
+++ util/concat.c
@@ -10,12 +10,12 @@ concat(FILE *fp1, const char *s1, FILE *fp2, const char *s2)
 	char buf[BUFSIZ];
 	size_t n;
 
-	while((n = fread(buf, 1, sizeof buf, fp1)) > 0) {
-		if(fwrite(buf, 1, n, fp2) != n)
+	while ((n = fread(buf, 1, sizeof buf, fp1)) > 0) {
+		if (fwrite(buf, 1, n, fp2) != n)
 			eprintf("%s: write error:", s2);
+		if (feof(fp1))
+			break;
 	}
-
-	if(ferror(fp1))
+	if (ferror(fp1))
 		eprintf("%s: read error:", s1);
 }
-
