a1784b721d19c7f1743e1779f3037cd15b91acf5
sin <sin@2f30.org>
2014-09-29 14:10:54 +0100

    Fix build error
    Remove __dead.

--- expr.c
+++ expr.c
@@ -23,7 +23,7 @@ int		 to_integer(struct val *);
 void		 to_string(struct val *);
 int		 is_zero_or_null(struct val *);
 void		 nexttoken(int);
-__dead void	 error(void);
+void	 	 error(void);
 struct val	*eval6(void);
 struct val	*eval5(void);
 struct val	*eval4(void);
@@ -219,7 +219,7 @@ nexttoken(int pat)
 	return;
 }
 
-__dead void
+void
 error(void)
 {
 	errx(2, "syntax error");
