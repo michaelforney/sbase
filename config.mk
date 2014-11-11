a8c31586c52e83366547908acb53300f2665ac3c
sin <sin@2f30.org>
2014-11-11 18:15:37 +0000

    Revert "config.mk: make cc the default $CC"
    This reverts commit d2b527e369685606f812233a24dab12188d8930c.

--- config.mk
+++ config.mk
@@ -5,16 +5,15 @@ VERSION = 0.0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
+#CC = gcc
+#CC = musl-gcc
+LD = $(CC)
 CPPFLAGS = -D_BSD_SOURCE -D_GNU_SOURCE
 CFLAGS   = -g -std=c99 -Wall -pedantic $(CPPFLAGS)
 LDFLAGS  = -g
 
-#CC = gcc
 #CC = tcc
-#CC = musl-gcc
-CC = cc
-LD = $(CC)
-
+#LD = $(CC)
 #CPPFLAGS = -D_POSIX_C_SOURCE=200112L
 #CFLAGS   = -Os -Wall $(CPPFLAGS) -D_GNU_SOURCE
 #LDFLAGS  =
