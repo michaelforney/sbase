0bb5f5f45352a4011627ed71d222a224e504669a
FRIGN <dev@frign.de>
2015-01-25 22:44:47 +0100

    Add mandoc-manpage for link(1)
    and mark it as finished in the README.

--- link.c
+++ link.c
@@ -10,7 +10,7 @@ main(int argc, char *argv[])
 	argv0 = argv[0];
 
 	if (argc != 3)
-		eprintf("usage: %s target linkname\n", argv0);
+		eprintf("usage: %s target name\n", argv0);
 	if (link(argv[1], argv[2]) < 0)
 		eprintf("link:");
 	return 0;
