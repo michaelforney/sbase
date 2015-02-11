51680535ce4d929a40e2ffa85d64c2aa6f57f90f
sin <sin@2f30.org>
2015-02-11 00:27:30 +0000

    getlines: Style fix

--- libutil/getlines.c
+++ libutil/getlines.c
@@ -20,13 +20,13 @@ getlines(FILE *fp, struct linebuf *b)
 			b->lines = nline;
 		}
 		linelen = len + 1;
-		b->lines[b->nlines-1] = emalloc(linelen);
-		memcpy(b->lines[b->nlines-1], line, linelen);
+		b->lines[b->nlines - 1] = emalloc(linelen);
+		memcpy(b->lines[b->nlines - 1], line, linelen);
 	}
 	free(line);
-	if (strchr(b->lines[b->nlines-1], '\n') == NULL) {
-		b->lines[b->nlines-1] = erealloc(b->lines[b->nlines-1], linelen + 1);
-		b->lines[b->nlines-1][linelen-1] = '\n';
-		b->lines[b->nlines-1][linelen] = '\0';
+	if (!strchr(b->lines[b->nlines - 1], '\n')) {
+		b->lines[b->nlines - 1] = erealloc(b->lines[b->nlines - 1], linelen + 1);
+		b->lines[b->nlines - 1][linelen - 1] = '\n';
+		b->lines[b->nlines - 1][linelen] = '\0';
 	}
 }
