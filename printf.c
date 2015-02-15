92b14e9c7770bf679da3fa8551f906d46a9a39c3
sin <sin@2f30.org>
2015-02-15 16:00:32 +0000

    Use BSD style function definitions

--- printf.c
+++ printf.c
@@ -8,12 +8,14 @@
 #include "utf.h"
 #include "util.h"
 
-static void usage(void)
+static void
+usage(void)
 {
 	eprintf("%s format [arg ...]\n", argv0);
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
 	Rune *rarg;
 	size_t i, j, argi, lastargi, formatlen, arglen;
