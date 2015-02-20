84924d050ef0a471207b775744a60fc71102cef8
sin <sin@2f30.org>
2015-02-20 16:24:17 +0000

    Add info about NetBSD to config.mk

--- config.mk
+++ config.mk
@@ -10,6 +10,7 @@ LD = $(CC)
 AR = ar
 RANLIB = ranlib
 
+# For NetBSD add -D_NETBSD_SOURCE
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700
 CFLAGS   = -std=c99 -Wall -pedantic
 LDFLAGS  = -s # -lrt
