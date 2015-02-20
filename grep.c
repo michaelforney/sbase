71de7813c02d9fc3252a9a791b869de767202db8
sin <sin@2f30.org>
2015-02-20 16:00:39 +0000

    Include strings.h for strcasecmp()
    Fixes another build error on NetBSD.

--- grep.c
+++ grep.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include "queue.h"
 #include "util.h"
--- logger.c
+++ logger.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #define SYSLOG_NAMES
 #include <syslog.h>
 #include <unistd.h>
