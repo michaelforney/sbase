7cb966c170bc5094ecbc5874ac9afd69a51ad5df
FRIGN <dev@frign.de>
2015-03-03 17:24:14 +0100

    Audit pwd(1)
    Nothing special
    1) Update manpage to current style.
    2) Reorder functions
    3) Logical trickery in getpwd()

--- pwd.c
+++ pwd.c
@@ -6,7 +6,19 @@
 
 #include "util.h"
 
-static const char *getpwd(const char *cwd);
+static const char *
+getpwd(const char *cwd)
+{
+	const char *pwd;
+	struct stat cst, pst;
+
+	if (!(pwd = getenv("PWD")) || pwd[0] != '/' || stat(pwd, &pst) < 0)
+		return cwd;
+	if (stat(cwd, &cst) < 0)
+		eprintf("stat %s:", cwd);
+
+	return (pst.st_dev == cst.st_dev && pst.st_ino == cst.st_ino) ? pwd : cwd;
+}
 
 static void
 usage(void)
@@ -33,20 +45,4 @@ main(int argc, char *argv[])
 	puts((mode == 'L') ? getpwd(cwd) : cwd);
 
 	return 0;
-}
-
-static const char *
-getpwd(const char *cwd)
-{
-	const char *pwd;
-	struct stat cst, pst;
-
-	if (!(pwd = getenv("PWD")) || pwd[0] != '/' || stat(pwd, &pst) < 0)
-		return cwd;
-	if (stat(cwd, &cst) < 0)
-		eprintf("stat %s:", cwd);
-	if (pst.st_dev == cst.st_dev && pst.st_ino == cst.st_ino)
-		return pwd;
-	else
-		return cwd;
 }
