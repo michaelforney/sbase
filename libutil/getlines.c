michaelforney/sbase

commit 66a5ea722d18fc76ce7c5c4323e8cfebe58e7517

getlines: last line of file should always have a newline
This is a useful behavior if you want to reorder the lines,
because otherwise you might end up with originally two lines
on one, e.g.

	$ echo -ne "foo\nbar" | sort
	barfoo

libutil/getlines.c
@@ -24,4 +24,9 @@ getlines(FILE *fp, struct linebuf *b)
 		memcpy(b->lines[b->nlines-1], line, linelen);
 	}
 	free(line);
+	if (strchr(b->lines[b->nlines-1], '\n') == NULL) {
+		b->lines[b->nlines-1] = erealloc(b->lines[b->nlines-1], linelen + 1);
+		b->lines[b->nlines-1][linelen-1] = '\n';
+		b->lines[b->nlines-1][linelen] = '\0';
+	}
 }

commit 9f974301438f0063637e1330cdb829df12cd5081

libutil/getlines: fix crash with no lines
because b->lines and b->nlines would be 0 with no lines read.

reproduce: printf '' | sort or cols

bug was introduced by commit: 66a5ea722d18fc76ce7c5c4323e8cfebe58e7517

libutil/getlines.c
@@ -9,20 +9,21 @@
 void
 getlines(FILE *fp, struct linebuf *b)
 {
-	char *line = NULL, **nline;
+	char *line = NULL;
 	size_t size = 0, linelen;
 	ssize_t len;
 
 	while ((len = getline(&line, &size, fp)) > 0) {
 		if (++b->nlines > b->capacity) {
 			b->capacity += 512;
-			nline = erealloc(b->lines, b->capacity * sizeof(*b->lines));
-			b->lines = nline;
+			b->lines = erealloc(b->lines, b->capacity * sizeof(*b->lines));
 		}
 		linelen = len + 1;
 		b->lines[b->nlines - 1] = memcpy(emalloc(linelen), line, linelen);
 	}
 	free(line);
+	if(!b->nlines || !b->lines)
+		return;
 	if (!strchr(b->lines[b->nlines - 1], '\n')) {
 		b->lines[b->nlines - 1] = erealloc(b->lines[b->nlines - 1], linelen + 1);
 		b->lines[b->nlines - 1][linelen - 1] = '\n';