d38361245097d1903eb9ef55c843bdef6a621f38
FRIGN <dev@frign.de>
2015-06-05 01:16:13 +0200

    Add some info to config.mk
    glibc breaks tools on 32bit systems due to a design flaw not allowing
    64bit file offsets and only doing so with a cryptic flag.
    
    Also, make sure to tell users about -lrt, which might be needed on
    some systems.

--- config.mk
+++ config.mk
@@ -10,7 +10,9 @@ LD = $(CC)
 AR = ar
 RANLIB = ranlib
 
-# For NetBSD add -D_NETBSD_SOURCE
+# for NetBSD add -D_NETBSD_SOURCE
+# for glibc on 32bit add -D_FILE_OFFSET_BITS=64 or use something sane
+# -lrt might be needed on some systems
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700
 CFLAGS   = -std=c99 -Wall -pedantic
 LDFLAGS  = -s
