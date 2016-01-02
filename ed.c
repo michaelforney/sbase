michaelforney/sbase

commit b19d708923b28cfc84a963b6eb59427b3c09907c

ed: add init()
These funcction initializes the scratch buffer, and in case of
having a file name parameter it loads the file in the scratch.
It also fixes a problem before this version, where the saved
filename was not set when the file didn't exist.

ed.c
@@ -629,12 +629,6 @@ doread(char *fname)
 	fp = NULL;
 	if (fclose(aux))
 		error("input/output error");
-
-	if (savfname[0] == '\0') {
-		modflag = 0;
-		clearundo();
-		strcpy(savfname, fname);
-	}
 }
 
 static void
@@ -1359,6 +1353,23 @@ edit(void)
 	}
 }
 
+static void
+init(char *fname)
+{
+	size_t len;
+
+	if (setjmp(savesp))
+		return;
+	setscratch();
+	if (!fname)
+		return;
+	if ((len = strlen(fname)) >= FILENAME_MAX || len == 0)
+		error("incorrect filename");
+	memcpy(savfname, fname, len);
+	doread(fname);
+	clearundo();
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -1380,15 +1391,8 @@ main(int argc, char *argv[])
 	signal(SIGINT, sigintr);
 	signal(SIGHUP, sighup);
 	signal(SIGQUIT, SIG_IGN);
-	if (!setjmp(savesp)) {
-		setscratch();
-		if (*argv) {
-			if (strlen(*argv) >= FILENAME_MAX)
-				error("file name too long");
-			doread(*argv);
-		}
-	}
 
+	init(*argv);
 	edit();
 
 	/* not reached */

commit cf0a0fc940b33a9213993448e2d2d8cf217108b0

ed: Fix error introduced in b19d708
This patch introduced init() function, which removed the initialization
code of savfname in doread, but this is incorrect, because savfname
can be initialized with a r command if savfname is empty.

ed.c
@@ -702,7 +702,7 @@ chkprint(int flag)
 }
 
 static char *
-getfname(void)
+getfname(char comm)
 {
 	int c;
 	char *bp;
@@ -721,6 +721,8 @@ getfname(void)
 		error("file name too long");
 	} else {
 		*bp = '\0';
+		if (savfname[0] == '\0' || comm == 'e' || comm == 'f')
+			strcpy(savfname, fname);
 		return fname;
 	}
 	return NULL; /* not reached */
@@ -1015,7 +1017,7 @@ subst(int nth)
 static void
 docmd(void)
 {
-	char *s, cmd;
+	char cmd;
 	int rep = 0, c, line3, num, trunc;
 
 repeat:
@@ -1073,13 +1075,13 @@ repeat:
 		trunc = 1;
 	case 'W':
 		deflines(nextln(0), lastln);
-		dowrite(getfname(), trunc);
+		dowrite(getfname(cmd), trunc);
 		break;
 	case 'r':
 		if (nlines > 1)
 			goto bad_address;
 		deflines(lastln, lastln);
-		doread(getfname());
+		doread(getfname(cmd));
 		break;
 	case 'd':
 		chkprint(1);
@@ -1190,10 +1192,11 @@ repeat:
 	case 'f':
 		if (nlines > 0)
 			goto unexpected;
-		if (!strcmp(s = getfname(), savfname))
-			puts(savfname);
+		if (back(input()) != '\n')
+			getfname(cmd);
 		else
-			strcpy(savfname, s);
+			puts(savfname);
+		chkprint(0);
 		break;
 	case 'E':
 		modflag = 0;
@@ -1202,7 +1205,7 @@ repeat:
 			goto unexpected;
 		if (modflag)
 			goto modified;
-		strcpy(savfname, getfname());
+		getfname(cmd);
 		setscratch();
 		deflines(curln, curln);
 		doread(savfname);