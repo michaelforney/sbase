3debc5e064987974f0dfc098f29ae6a48b712a4d
FRIGN <dev@frign.de>
2016-03-07 11:00:47 +0100

    Add linecmp()

---
+++ libutil/linecmp.c
@@ -0,0 +1,20 @@
+/* See LICENSE file for copyright and license details. */
+#include <stdio.h>
+#include <string.h>
+
+#include "../text.h"
+#include "../util.h"
+
+int
+linecmp(struct line *a, struct line *b)
+{
+	int res = 0;
+
+	if (!(res = memcmp(a->data, b->data, MIN(a->len, b->len))) &&
+	    a->len != b->len) {
+		res = a->data[MIN(a->len, b->len) - 1] -
+		      b->data[MIN(a->len, b->len) - 1];
+	}
+
+	return res;
+}
