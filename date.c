4b13b23a689da5fc01c2a26affe8ad8087c777bf
John Vogel <jvogel4@stny.rr.com>
2017-01-01 20:09:53 -0500

    date: add date/time setting capability
    [Michael Forney: Moved functionality to setdate, other minor tweaks]

--- date.c
+++ date.c
@@ -1,6 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include <time.h>
 
 #include "util.h"
@@ -8,7 +10,53 @@
 static void
 usage(void)
 {
-	eprintf("usage: %s [-u] [-d time] [+format]\n", argv0);
+	eprintf("usage: %s [-u] [-d time] [+format | mmddHHMM[[CC]yy]]\n", argv0);
+}
+
+static int
+datefield(const char *s, size_t i)
+{
+	if (!isdigit(s[i]) || !isdigit(s[i+1]))
+		eprintf("invalid date format: %s\n", s);
+
+	return (s[i] - '0') * 10 + (s[i+1] - '0');
+}
+
+static void
+setdate(const char *s, struct tm *now)
+{
+	struct tm date;
+	struct timespec ts;
+
+	switch (strlen(s)) {
+	case 8:
+		date.tm_year = now->tm_year;
+		break;
+	case 10:
+		date.tm_year = datefield(s, 8);
+		if (date.tm_year < 69)
+			date.tm_year += 100;
+		break;
+	case 12:
+		date.tm_year = ((datefield(s, 8) - 19) * 100) + datefield(s, 10);
+		break;
+	default:
+		eprintf("invalid date format: %s\n", s);
+		break;
+	}
+
+	date.tm_mon = datefield(s, 0) - 1;
+	date.tm_mday = datefield(s, 2);
+	date.tm_hour = datefield(s, 4);
+	date.tm_min = datefield(s, 6);
+
+	ts.tv_sec = mktime(&date);
+	if (ts.tv_sec == (time_t)-1)
+		eprintf("mktime:");
+	ts.tv_nsec = 0;
+
+	if (clock_settime(CLOCK_REALTIME, &ts) == -1)
+		eprintf("clock_settime:");
 }
 
 int
@@ -18,8 +66,6 @@ main(int argc, char *argv[])
 	struct tm *(*tztime)(const time_t *) = localtime;
 	time_t t;
 	char buf[BUFSIZ], *fmt = "%c", *tz = "local";
-
-	t = time(NULL);
 
 	ARGBEGIN {
 	case 'd':
@@ -33,14 +79,21 @@ main(int argc, char *argv[])
 		usage();
 	} ARGEND
 
-	if (argc) {
-		if (argc != 1 || argv[0][0] != '+')
-			usage();
-		else
-			fmt = &argv[0][1];
-	}
+	t = time(NULL);
+	if (t == (time_t)-1)
+		eprintf("time failed:");
 	if (!(now = tztime(&t)))
 		eprintf("%stime failed\n", tz);
+
+	if (argc) {
+		if (argc != 1)
+			usage();
+		if (argv[0][0] != '+') {
+			setdate(argv[0], now);
+			return 0;
+		}
+		fmt = &argv[0][1];
+	}
 
 	strftime(buf, sizeof(buf), fmt, now);
 	puts(buf);
