2f0b15201d182f25dcb75891fc43190760d7529a
Richard Ipsum <richardipsum@vx21.xyz>
2020-04-15 17:46:39 +0100

    paste: Support -d '\0'
    POSIX specifies that -d '\0' sets the delimiter to an empty string.

--- utf.h
+++ utf.h
@@ -38,6 +38,7 @@ int fullrune(const char *, size_t);
 char *utfecpy(char *, char *, const char *);
 size_t utflen(const char *);
 size_t utfnlen(const char *, size_t);
+size_t utfmemlen(const char *, size_t);
 char *utfrune(const char *, Rune);
 char *utfrrune(const char *, Rune);
 char *utfutf(const char *, const char *);
@@ -60,6 +61,7 @@ Rune tolowerrune(Rune);
 Rune toupperrune(Rune);
 
 size_t utftorunestr(const char *, Rune *);
+size_t utfntorunestr(const char *, size_t, Rune *);
 
 int fgetrune(Rune *, FILE *);
 int efgetrune(Rune *, FILE *, const char *);
